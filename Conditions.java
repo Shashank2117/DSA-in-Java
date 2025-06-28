e52faa707a5e046122b91b37d7dd8305cd8bf254
Shashank kanojiya <kanojiyashashank2004@gmail.com>
2025-06-28 18:37:11 +0530

    Late fee calculator for books

--- Conditions.java
+++ Conditions.java
@@ -1,33 +1,50 @@
+// 1.	Library Late Fees: Write a Java program to calculate the late fees for returning library books. 
+//      Prompt the user to input the number of days overdue and the type of book (regular, fiction, or reference).
+//      Determine the late fee according to the following rules:
+// •	For regular books: $0.50 per day
+// •	For fiction books: $0.75 per day
+// •	For reference books: No late fee
+
+import java.util.Scanner;
+
 public class Conditions{
     public static void main(String[] args) {
-		
-		System.out.println("Calculating the late fees of returning books --");
-		
-		System.out.println();
-		
-		System.out.println("Enter the number of days overdue :");
-		Scanner sc1 = new Scanner(System.in);
-		int days = sc1.nextInt();
-		
-		System.out.println();
-		
-	    System.out.println("Enter the type of book (regular, fictional, reference) :");
-	    Scanner sc2 = new Scanner(System.in);
-	    String book = sc2.nextLine();
-	    
-	    System.out.println();
-	    
-	    if(book.equalsIgnoreCase("regular") || book.equalsIgnoreCase("Regular")) {
-	    	double lateFees = days * 0.50d;
-	    	System.out.println("Late fees for Regular book for "+ days + " days is : " + lateFees + " $");
-	    }else if(book.equalsIgnoreCase("fictional") || book.equalsIgnoreCase("Fictional")) {
-	    	double lateFees = days * 0.70d;
-	    	System.out.println("Late fees for Fictional book for "+ days + " days is : " + lateFees + " $");
-	    }else if(book.equalsIgnoreCase("reference") || book.equalsIgnoreCase("Reference")) {
-	    	System.out.println("No late fees for Reference books");
-	    }else {
-	    	System.out.println("Wrong Book Type");
-	    }
+        System.out.println("Calculating Late Fes for Books __");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println();
+
+        System.out.println("Enter type of book (regular, fiction, or reference) ");
+        String bookType = sc.nextLine();
+
+        System.out.println();
+
+        System.out.println("Number of day Overdue ");
+        int days = sc.nextInt();
+
+        System.out.println();
+
+        double amount = 0;
+
+        if(bookType.equalsIgnoreCase("Reference")){
+
+            System.out.println("No Late Fees for reference books.");
+
+        }else if (bookType.equalsIgnoreCase("Regular")) {
+
+            if(days > 0){
+                amount = 0.50 * days;
+                System.out.println(amount + "$ as Late fee for Regular Boos");
+            }
+        }else if(bookType.equalsIgnoreCase("Fiction")){
+
+            if(days > 0){
+                amount = 0.75 * days;
+                System.out.println(amount + "$ as Late fee for Fiction Books");
+            }
+        }else{
+            System.out.println("Invalid Type of Book.");
+        }
+        
     }
 }
-
