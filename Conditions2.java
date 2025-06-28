ec9d44f5ef2676b7aec1b25f1cde0738302c7905
Shashank kanojiya <kanojiyashashank2004@gmail.com>
2025-06-28 19:02:15 +0530

    Calculating Grades of Student

--- Conditions2.java
+++ Conditions2.java
@@ -1,57 +1,58 @@
-//Q.	Grading System: Develop a Java program to determine the grade of a student based on their exam scores.
-//                      Prompt the user to input their exam scores for three subjects. 
-//                      Calculate the average score and determine their grade according to the following criteria:
-//•	Average score >= 90: Grade A
-//•	80 <= Average score < 90: Grade B
-//•	70 <= Average score < 80: Grade C
-//•	60 <= Average score < 70: Grade D
-//•	Average score < 60: Grade F
+// 2.	Grading System: Develop a Java program to determine the grade of a student based on their exam scores.
+//      Prompt the user to input their exam scores for three subjects. 
+//      Calculate the average score and determine their grade according to the following criteria:
+// •	Average score >= 90: Grade A
+// •	80 <= Average score < 90: Grade B
+// •	70 <= Average score < 80: Grade C
+// •	60 <= Average score < 70: Grade D
+// •	Average score < 60: Grade F
+
 
 import java.util.Scanner;
-
 public class Conditions2 {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Result based on your performance");
-		
-		System.out.println();
-		
-//		physics marks input
-		System.out.println("Enter the marks of PHYSICS");
-		Scanner sc1 = new Scanner(System.in);
-		int physics = sc1.nextInt();
-		
-//		chemistry marks input
-		System.out.println("Enter the marks of CHEMISTRY");
-		Scanner sc2 = new Scanner(System.in);
-		int chemistry = sc2.nextInt();
-		
-//		maths marks input
-		System.out.println("Enter the marks of MATHS");
-		Scanner sc3 = new Scanner(System.in);
-		int maths = sc3.nextInt();
-		
-		System.out.println();
-		
-		System.out.println("According to the marks your final Result is :");
-		
-		int score = physics + chemistry + maths;
-		int averageScore = score/3;
-		
-		if(averageScore >= 90 && averageScore<= 100) {
-			System.out.println("A+ Grade");
-		}else if(averageScore >= 80 && averageScore < 90) {
-			System.out.println("B Grade");
-		}else if(averageScore >= 70 && averageScore < 80) {
-			System.out.println("C Grade");
-		}else if(averageScore >= 60 && averageScore < 70) {
-			System.out.println("D Grade");
-		}else if(averageScore < 60) {
-			System.out.println("F Grade");
-		}else {
-			System.out.println("Wrong input");
-		}
-	}
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Calculating Grades");
+        System.out.println();
 
+        System.out.println("Enter Marks of Maths :");
+        int math = sc.nextInt();
+
+        System.out.println();
+
+        System.out.println("Enter Marks of Physics :");
+        int phy = sc.nextInt();
+
+        System.out.println();
+
+        System.out.println("Enter Marks of Chemistry :");
+        int chem = sc.nextInt();
+
+        System.out.println();
+
+        int average = (math + phy + chem) / 3;
+
+        if (average >= 90) {
+            System.out.println("A Grade");
+
+        }else if (average >= 80 && average < 90) {
+            System.out.println("B Grade");
+
+        }else if (average >= 70 && average < 80) {
+            System.out.println("C Grade");
+
+        }else if (average >= 60 && average < 70) {
+            System.out.println("D Grade");
+
+        }else if (average >= 0 && average > 60) {
+            System.out.println("Fail");
+
+        }else{
+            System.out.println("Please Enter Valid Number of Subjects ");
+        }
+
+
+    }
 }
