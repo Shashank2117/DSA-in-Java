eb23210adbd71bfc95a6ade0dd10db041e5de82b
Shashank kanojiya <kanojiyashashank2004@gmail.com>
2025-06-29 19:16:18 +0530

    Reverse the number

---
+++ Loops.java
@@ -0,0 +1,9 @@
+// Print Numbers in Reverse: Write a Java program to print the numbers from 10 down to 1
+
+public class Loops {
+    public static void main(String[] args) {
+        for(int i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}
