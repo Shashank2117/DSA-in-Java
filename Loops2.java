b5622056180b45cacef233d28f950ad49d4002d0
Shashank kanojiya <kanojiyashashank2004@gmail.com>
2025-06-29 19:17:08 +0530

    Sum of odd numbers

---
+++ Loops2.java
@@ -0,0 +1,14 @@
+// Sum of Odd Numbers: Develop a Java program to calculate the sum of all odd numbers from 1 to 100.
+
+public class Loops2 {
+
+    static void sum(int n){
+        System.out.println(n * n);
+    }
+
+
+    public static void main(String[] args) {
+        sum(100);
+    }
+    
+}
