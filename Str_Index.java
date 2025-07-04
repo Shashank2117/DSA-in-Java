cbddbdf5c73738d0265b84c78b9306b5d7fca3dd
Shashank kanojiya <kanojiyashashank2004@gmail.com>
2025-07-04 22:03:41 +0530

    Basics String questions

---
+++ Str_Index.java
@@ -0,0 +1,45 @@
+
+
+public class Str_Index {
+    public static void main(String[] args) {
+        
+        // String name = "Shashank";
+
+        // printing string characters
+        // for(int i=0; i<name.length(); i++){
+        //     System.out.print(name.charAt(i) + " ");
+        // }
+
+        // System.out.println();
+
+
+        // printing in reverse
+        // for(int i=name.length()-1; i>=0; i--){
+        //     System.out.print(name.charAt(i) + " ");
+        // }
+
+
+
+
+        // removing duplicate
+
+        // String name = "malyalam";
+        // String result = "";
+
+        // for(char i=0; i<name.length(); i++){
+        //     char ch = name.charAt(i);
+
+        //     if(result.indexOf(ch) == -1){
+        //         result += ch;
+        //     }
+        // }
+        // System.out.println(result);
+        
+     
+        String name = "helloo";
+        String repeat = "";
+
+        
+
+    }
+}
