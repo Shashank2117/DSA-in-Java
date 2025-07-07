dbbf86810ca60ee56f2bbb9be74502c5c94ab6dc
Shashank kanojiya <kanojiyashashank2004@gmail.com>
2025-07-07 22:11:06 +0530

    Checking pair equals to target

--- targetSum.java
+++ targetSum.java
@@ -1,31 +1,22 @@
 public class targetSum {
+        public static void main(String[] args) {
 
-    void sum(){
         int []arr = {1,4,5,2};
         int target = 7;
 
         for(int i=0; i<arr.length; i++){
-            for(int j=0; j<=i; j++){
-                // int sum = arr[i] + arr[j];
+
+            for(int j=i+1; j<arr.length; j++){
+
                 if(arr[i] + arr[j] == target){
-                    // System.out.println(arr[i] + arr[j]);
+
+                    System.out.println(arr[i] + " " + arr[j]);
                     
                 }
+                                                          
             }
         }
     }
-
-
-
-    void Max(){
-        int arr[] = {1,1,-1,-2,3,4};
-
-    }
-
-
-    public static void main(String[] args) {
-        targetSum tg = new targetSum();
-        tg.sum();
-        tg.Max();
-    }
+    
 }
+
